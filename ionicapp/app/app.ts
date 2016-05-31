07201a5e00a402da76bbc942edec4c6e4c9cee46
Manuel Taber <manuel.taber@topcontrol.local>
2016-05-31 18:13:53 +0200

    new ionic module area-selection

--- ionicapp/app/app.ts
+++ ionicapp/app/app.ts
@@ -3,6 +3,7 @@ import {App, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
+import {AreaSelection} from './pages/area-selection/area-selection';
 
 
 @App({
@@ -25,7 +26,8 @@ class MyApp {
     // set our app's pages
     this.pages = [
       { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'My First List', component: ListPage },
+      { title: 'AreaSelection', component: AreaSelection },
     ];
   }
 
