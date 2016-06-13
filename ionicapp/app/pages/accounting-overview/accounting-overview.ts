25909ab886b36ecb62485063f7fbdfb207c161ce
Manuel Taber <manuel.taber@topcontrol.local>
2016-06-13 11:02:44 +0200

    added checkboxes to page accounting-overview

--- ionicapp/app/pages/accounting-overview/accounting-overview.ts
+++ ionicapp/app/pages/accounting-overview/accounting-overview.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams, Alert} from 'ionic-angular';
 import {Area} from '../../classes/area';
 import {Item} from '../../classes/item';
 import {Order} from '../../classes/order';
@@ -12,15 +12,67 @@ import {TranslationService} from '../../services/translation.service';
 export class AccountingOverviewPage {
   
   area: Area;
-  items: Item[];
+  accountingItems: AccountingItem[] = [];
   searching: boolean;
 
   constructor(private nav: NavController, private navParams: NavParams,
               private orderService: OrderService,
               private translationService: TranslationService) {
     this.area = navParams.get('area');
-    this.items = orderService.getOpenOrdersItemsByArea(this.area);
+    let items = this.orderService.getOpenOrdersItemsByArea(this.area);
+    for (let item of items) {
+      let accountingItem = new AccountingItem();
+      accountingItem.item = item;
+      accountingItem.selected = false;
+      this.accountingItems.push(accountingItem);
+    }
     this.searching = false;
   }
 
+  getSelectedItemCount() {
+    let count: number = 0;
+    for (let accountingItem of this.accountingItems) {
+      if (accountingItem.selected) {
+        count = count + 1;
+      }
+    }
+    return count;
+  }
+
+  getSelectedItemAmount() {
+    let amount: number = 0;
+    for (let accountingItem of this.accountingItems) {
+      if (accountingItem.selected) {
+        amount = amount + accountingItem.item.price;
+      }
+    }
+    return amount;
+  }
+
+  onCloseOrders(event) {
+    let confirm = Alert.create({
+      title: 'Achtung',
+      message: 'Sicher?',
+      buttons: [
+        {
+          text: 'Ja',
+          handler: () => {
+            //alert('Ja');
+          }
+        },
+        {
+          text: 'Nein',
+          handler: () => {
+            //alert('Nein');
+          }
+        }
+      ]
+    });
+    this.nav.present(confirm);
+  }
+}
+
+export class AccountingItem {
+  item: Item;
+  selected: boolean;
 }
