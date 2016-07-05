89e8dbf4437501695e5c43ba27e0052622cb7069
Manuel Taber <manuel.taber@topcontrol.local>
2016-07-05 17:43:26 +0200

    added optionals and ingredients to the mock data

--- ionicapp/app/mockups/categories.ts
+++ ionicapp/app/mockups/categories.ts
@@ -9,17 +9,20 @@ function getMockCategories(): Category[] {
     category.nr = mockCategory['nr'];
     category.desc = mockCategory['desc'];
     category.icon = mockCategory['icon'];
+    category.optionals = mockCategory['optionals'];
     mockCategories.push(category);
   }
   return mockCategories;
 }
 
 var MOCKCATEGORIES = [
-  { 'nr': 1, 'desc': 'Vorspeißen', 'icon': 'restaurant' },
-  { 'nr': 2, 'desc': 'Hauptspeißen', 'icon': 'restaurant' },
-  { 'nr': 3, 'desc': 'Nachspeißen', 'icon': 'ice-cream' },
-  { 'nr': 4, 'desc': 'Beilagen', 'icon': 'cafe' },
-  { 'nr': 5, 'desc': 'Getränke', 'icon': 'beer' },
-  { 'nr': 6, 'desc': 'Sonstige', 'icon': 'restaurant' },
-  { 'nr': 7, 'desc': 'Pizza', 'icon': 'pizza' }
+  { 'nr': 1, 'desc': 'Vorspeißen', 'icon': 'restaurant', 'optionals': [] },
+  { 'nr': 2, 'desc': 'Hauptspeißen', 'icon': 'restaurant', 'optionals': [] },
+  { 'nr': 3, 'desc': 'Nachspeißen', 'icon': 'ice-cream', 'optionals': [] },
+  { 'nr': 4, 'desc': 'Beilagen', 'icon': 'cafe', 'optionals': [] },
+  { 'nr': 5, 'desc': 'Getränke', 'icon': 'beer', 
+    'optionals': ['Eiswürfel', 'Zitrone'] },
+  { 'nr': 6, 'desc': 'Sonstige', 'icon': 'restaurant', 'optionals': [] },
+  { 'nr': 7, 'desc': 'Pizza', 'icon': 'pizza', 
+    'optionals': ['Knoblauchsauce', 'Vollkorn'] }
 ];
--- ionicapp/app/mockups/items.ts
+++ ionicapp/app/mockups/items.ts
@@ -17,43 +17,50 @@ function getMockItems(): Item[] {
         item.category = category;
       }
     }
+    item.ingredients = mockItem['ingredients'];
     mockItems.push(item);
   }
   return mockItems;
 }
 
 var MOCKITEMS = [
-  { 'nr': 1, 'desc': 'Nudel', 'price': 8, 'category_nr': 1 },
-  { 'nr': 2, 'desc': 'Suppe', 'price': 5, 'category_nr': 1 },
-  { 'nr': 3, 'desc': 'Reis', 'price': 6, 'category_nr': 1 },
-  { 'nr': 4, 'desc': 'Lasagne', 'price': 7.5, 'category_nr': 1 },
-  { 'nr': 5, 'desc': 'Knödel', 'price': 6.2, 'category_nr': 1 },
+  { 'nr': 1, 'desc': 'Nudel', 'price': 8, 'category_nr': 1, 'ingredients': [] },
+  { 'nr': 2, 'desc': 'Suppe', 'price': 5, 'category_nr': 1, 'ingredients': [] },
+  { 'nr': 3, 'desc': 'Reis', 'price': 6, 'category_nr': 1, 'ingredients': [] },
+  { 'nr': 4, 'desc': 'Lasagne', 'price': 7.5, 'category_nr': 1, 'ingredients': [] },
+  { 'nr': 5, 'desc': 'Knödel', 'price': 6.2, 'category_nr': 1, 'ingredients': [] },
 
-  { 'nr': 6, 'desc': 'Fleisch', 'price': 12.5, 'category_nr': 2 },
-  { 'nr': 7, 'desc': 'Fisch', 'price': 21, 'category_nr': 2 },
-  { 'nr': 8, 'desc': 'Wienerschnitzel', 'price': 12, 'category_nr': 2 },
+  { 'nr': 6, 'desc': 'Fleisch', 'price': 12.5, 'category_nr': 2, 'ingredients': [] },
+  { 'nr': 7, 'desc': 'Fisch', 'price': 21, 'category_nr': 2, 'ingredients': [] },
+  { 'nr': 8, 'desc': 'Wienerschnitzel', 'price': 12, 'category_nr': 2, 'ingredients': [] },
 
-  { 'nr': 9, 'desc': 'Kuchen', 'price': 4.5, 'category_nr': 3 },
-  { 'nr': 10, 'desc': 'Eis', 'price': 5, 'category_nr': 3 },
-  { 'nr': 11, 'desc': 'Strudel', 'price': 6, 'category_nr': 3 },
+  { 'nr': 9, 'desc': 'Kuchen', 'price': 4.5, 'category_nr': 3, 'ingredients': [] },
+  { 'nr': 10, 'desc': 'Eis', 'price': 5, 'category_nr': 3, 'ingredients': [] },
+  { 'nr': 11, 'desc': 'Strudel', 'price': 6, 'category_nr': 3, 'ingredients': [] },
 
-  { 'nr': 12, 'desc': 'Kartoffel', 'price': 4.5, 'category_nr': 4 },
-  { 'nr': 13, 'desc': 'Pommes', 'price': 3.5, 'category_nr': 4 },
-  { 'nr': 14, 'desc': 'Salat', 'price': 3, 'category_nr': 4 },
+  { 'nr': 12, 'desc': 'Kartoffel', 'price': 4.5, 'category_nr': 4, 'ingredients': [] },
+  { 'nr': 13, 'desc': 'Pommes', 'price': 3.5, 'category_nr': 4, 'ingredients': [] },
+  { 'nr': 14, 'desc': 'Salat', 'price': 3, 'category_nr': 4, 'ingredients': [] },
 
-  { 'nr': 15, 'desc': 'Bier', 'price': 3.5, 'category_nr': 5 },
-  { 'nr': 16, 'desc': 'Hefe', 'price': 5, 'category_nr': 5 },
-  { 'nr': 17, 'desc': 'Wein', 'price': 4.5, 'category_nr': 5 },
-  { 'nr': 18, 'desc': 'Saft', 'price': 3, 'category_nr': 5 },
-  { 'nr': 19, 'desc': 'Cola', 'price': 3, 'category_nr': 5 },
-  { 'nr': 20, 'desc': 'Wasser', 'price': 2.5, 'category_nr': 5 },
+  { 'nr': 15, 'desc': 'Bier', 'price': 3.5, 'category_nr': 5, 'ingredients': [] },
+  { 'nr': 16, 'desc': 'Hefe', 'price': 5, 'category_nr': 5, 'ingredients': [] },
+  { 'nr': 17, 'desc': 'Wein', 'price': 4.5, 'category_nr': 5, 'ingredients': [] },
+  { 'nr': 18, 'desc': 'Saft', 'price': 3, 'category_nr': 5, 'ingredients': [] },
+  { 'nr': 19, 'desc': 'Cola', 'price': 3, 'category_nr': 5, 'ingredients': [] },
+  { 'nr': 20, 'desc': 'Wasser', 'price': 2.5, 'category_nr': 5, 'ingredients': [] },
 
-  { 'nr': 21, 'desc': 'Chips', 'price': 2.5, 'category_nr': 6 },
+  { 'nr': 21, 'desc': 'Chips', 'price': 2.5, 'category_nr': 6, 'ingredients': [] },
 
-  { 'nr': 22, 'desc': 'Margherita', 'price': 5.5, 'category_nr': 7 },
-  { 'nr': 23, 'desc': 'Tonno', 'price': 7.5, 'category_nr': 7 },
-  { 'nr': 24, 'desc': 'Schinken/Pilze', 'price': 8, 'category_nr': 7 },
-  { 'nr': 25, 'desc': 'Tirol', 'price': 8.5, 'category_nr': 7 },
-  { 'nr': 26, 'desc': 'Salami', 'price': 7, 'category_nr': 7 },
-  { 'nr': 27, 'desc': 'Calzone', 'price': 7.5, 'category_nr': 7 },
+  { 'nr': 22, 'desc': 'Margherita', 'price': 5.5, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano'] },
+  { 'nr': 23, 'desc': 'Tonno', 'price': 7.5, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano', 'Tonno'] },
+  { 'nr': 24, 'desc': 'Schinken/Pilze', 'price': 8, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano', 'Schinken', 'Pilze'] },
+  { 'nr': 25, 'desc': 'Tirol', 'price': 8.5, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano', 'Pilze', 'Speck'] },
+  { 'nr': 26, 'desc': 'Salami', 'price': 7, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano', 'Salami'] },
+  { 'nr': 27, 'desc': 'Calzone', 'price': 7.5, 'category_nr': 7, 
+    'ingredients': ['Tomaten', 'Mozarella', 'Oregano', 'Topfen'] },
 ];
