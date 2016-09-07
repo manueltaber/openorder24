e5457cc8736c6f6a4406ade8aa79a3b6afae00d3
Manuel Taber <manuel.taber@topcontrol.local>
2016-09-07 22:38:37 +0200

    added some images to the intro tutorial

--- ionicapp/app/pages/intro/intro.ts
+++ ionicapp/app/pages/intro/intro.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
 import {DashboardPage} from '../dashboard/dashboard';
+
+import {AuthService} from '../../services/auth.service';
  
 @Component({
     templateUrl: 'build/pages/intro/intro.html'
@@ -12,10 +14,16 @@ export class IntroPage {
         pager: true
     };
 
-    constructor(private nav: NavController){
+    constructor(private nav: NavController,
+                private authService: AuthService){
     }
  
     goToHome(){
+        this.authService.login();
+        //this.nav.setRoot(DashboardPage);
+    }
+
+    goToDashboard(){
         this.nav.setRoot(DashboardPage);
     }
 }
