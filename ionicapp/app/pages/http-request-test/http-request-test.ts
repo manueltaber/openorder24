0fab3a886116026f3772d6d82a03e6dc7c3aa707
Manuel Taber <manuel.taber@topcontrol.local>
2016-09-15 13:41:50 +0200

    http request test page modified

--- ionicapp/app/pages/http-request-test/http-request-test.ts
+++ ionicapp/app/pages/http-request-test/http-request-test.ts
@@ -1,18 +1,40 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import {Http} from '@angular/http';
+import {AuthHttp} from 'angular2-jwt';
+import {AuthService} from '../../services/auth.service';
+import 'rxjs/add/operator/map';
+
 @Component({
   templateUrl: 'build/pages/http-request-test/http-request-test.html'
 })
 export class HttpRequestTestPage {
 
   url: string;
+  
+  message: string;
+  error: string;
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController, private http: Http, private authHttp: AuthHttp, private auth: AuthService) {
+    this.url = "http://localhost:3000";
   }
 
+
   sendRequest() {
-      
+      // Here we use authHttp to make an authenticated
+      // request to the server. Change the endpoint up for
+      // one that points to your own server.
+      this.authHttp.get(this.url)
+        .map(res => res.json())
+        .subscribe(
+          data => this.message = data,
+          err => this.error = err
+        );
+  }
+
+  securedPing() {
+    
   }
 
 }
