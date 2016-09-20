9f6f990a90dce494d10e8c0f4ff794ccaf4cbb43
Manuel Taber <manuel.taber@topcontrol.local>
2016-09-20 21:01:45 +0200

    first backend api call

--- ionicapp/app/services/area.service.ts
+++ ionicapp/app/services/area.service.ts
@@ -1,27 +1,53 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {AuthHttp} from 'angular2-jwt';
+
 import {Area} from '../classes/area'
 import {getMockAreas} from '../mockups/areas';
 
 @Injectable()
 export class AreaService {
 
-  private areas: Area[];
+    private areas: Area[];
+    private loadedAreas: Area[];
 
-  constructor() {
-    this.areas = getMockAreas();
-  }
-  
-  getAreas(): Area[] {
-    return this.areas;
-  }
-  
-  getAreaByNr(nr: number): Area {
-    let area: Area;
-    for (area of this.areas) {
-      if (area.nr == nr) {
-        return area;
-      }
+    constructor(private http: Http, private authHttp: AuthHttp) {
+        this.areas = getMockAreas();
+        this.loadAreas();
     }
-  }
+  
+    public getAreas(): Area[] {
+        return this.areas;
+    }
+  
+    public getAreaByNr(nr: number): Area {
+        let area: Area;
+        for (area of this.areas) {
+            if (area.nr == nr) {
+                return area;
+            }
+        }
+    }
+
+    public loadAreas() {
+        this.authHttp.get("https://openorder24.herokuapp.com/api/v0/areas/get")
+            .map(res => res.json())
+            .subscribe(
+                data => this.loadedAreas = data,
+                err => console.log(err)
+            );
+    }
+
+    public createArea(area: Area) {
+        console.log("createArea");
+    }
+
+    public updateArea(area: Area) {
+        console.log("updateArea");
+    }
+
+    public deleteArea(area: Area) {
+        console.log("deleteArea");
+    }
 
 }
